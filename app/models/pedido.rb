84e338b959203345ec3d6d5329eb65f2e5df78c0
Fausto De La Torre <faustodelatog@gmail.com>
2016-05-24 00:46:41 -0500

    se agrega paypal como forma de pago

--- app/models/pedido.rb
+++ app/models/pedido.rb
@@ -4,10 +4,23 @@ class Pedido < ActiveRecord::Base
   end
 
   def total_items
-    items_json.reduce(0) {|sum, item| sum + item.producto.precio.to_f*item.cantidad.to_i }
+    items_json.reduce(0) {|sum, item| sum + item.producto.precio.to_f*item.cantidad.to_i }.round(2)
   end
 
   def total
     (total_items + costo_entrega.to_f - descuento.to_f).round(2)
+  end
+
+  def numero_items
+    items_json.reduce(0) {|sum, item| sum + item.cantidad.to_i }
+  end
+
+  def descripcion
+    descripcion = ''
+    items_json.each do |i|
+      descripcion += "#{i.cantidad} #{i.producto.nombre} - "
+    end
+    descripcion += "**"
+    descripcion.gsub('- **', '')
   end
 end
