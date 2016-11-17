433bcc8bf188ec4b4a4b674f9ad49855b321e9d8
Daniela Lopez <daniela.lopez.mino@gmail.com>
2016-11-16 22:29:34 -0500

    arreglos menores

--- app/models/pedido.rb
+++ app/models/pedido.rb
@@ -5,7 +5,7 @@ class Pedido < ActiveRecord::Base
 
   def total_items
     # items_json.reduce(0) {|sum, item| sum + item.producto.precio_original.to_f*item.cantidad.to_i }.round(2)
-    (total_con_descuento*1.15).round(2)
+    (total_items_con_descuento*1.15).round(2)
   end
 
   def total_items_con_descuento
