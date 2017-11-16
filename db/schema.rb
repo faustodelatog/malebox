f79d3373f185e2b28154199cd71246a795ec5420
Fausto De La Torre <ftorre@thoughtworks.com>
2017-11-16 18:30:01 -0500

    se revierte el cambio anterior

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170503000941) do
+ActiveRecord::Schema.define(version: 20171116204144) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
