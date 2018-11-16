493e8ceabf5c3457457d267c46f1d84dbbda65b5
Daniela Lopez <daniela.lopez.mino@gmail.com>
2018-11-16 12:32:22 -0500

    se cambian los estados

--- app/models/producto_cosa.rb
+++ app/models/producto_cosa.rb
@@ -4,6 +4,6 @@ class ProductoCosa < ActiveRecord::Base
 
   def quitar_de_inventario(inventario_id, pedido_id)
   	inventario_cosa = InventarioCosa.find_by(inventario: inventario_id, cosa_id: cosa.id)
-  	inventario_cosa.quitar(cantidad, "Pedido: #{pedido_id} - Producto: #{producto.id}: #{producto.nombre}")
+  	inventario_cosa.quitar(cantidad, "Pedido: #{pedido_id} - Producto: #{producto.id}: #{producto.nombre}") if inventario_cosa
   end
 end
